5d2edb967e9f420eaf1326ace13c7de8d8a0b5d0
Eduard Schäfer <eschaefer@easy-smarthome.de>
2020-01-16 20:21:31 +0100

    initial commit

---
+++ ShareBugRepro/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ShareBugRepro
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var fn = "Attachment.txt";
+            var file = Path.Combine(FileSystem.CacheDirectory, fn);
+            File.WriteAllText(file, "Hello World");
+
+            await Share.RequestAsync(new ShareFileRequest(fn, new ShareFile(file, "text/*")));
+        }
+    }
+}
